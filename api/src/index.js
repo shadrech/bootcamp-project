825d7e60227010ae9fa4939c2861dcf7d8a8742e
Mthabisi Nyathi <mthanyathi100@gmail.com>
2023-06-23 16:39:23 +0200

    joi added

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const studentModel = require('./models/students');
 const bodyParser = require('body-parser');
-
+const Joi = require('joi');
 const Mthaapp = express();
+const validator = require('express-joi-validation').createValidator({})
 
 Mthaapp.use(bodyParser.json());
+
+const studentSchema = Joi.object({
+  name: Joi.string().required(),
+  age: Joi.number().integer().strict().required(),
+  email: Joi.string().email().required()
+});
 
 Mthaapp.get('/student', (req, res) => {
   const result = studentModel.getStudents();
@@ -17,10 +24,15 @@ Mthaapp.get('/student', (req, res) => {
 // });
 
 Mthaapp.post('/student', (req, res) => {
-  const createdStudents = studentModel.postStudents(req.body);
-  res.json(createdStudents); 
-});
+  const { error } = studentSchema.validate(req.body);
 
+  if (error) {
+    res.status(400).json({ error: error.details[0].message });
+  } else {
+    const createdStudents = studentModel.postStudents(req.body);
+    res.json(createdStudents);
+  }
+});
 Mthaapp.put('/student/:id', (req, res) => {
   const { id } = req.params;
   const result = studentModel.putStudents(Number(id), req.body);
--- api/src/models/students.js
+++ api/src/models/students.js
@@ -1,26 +1,33 @@
 const students = [
     {
       id: 1,
+
       name: 'Mthabisi Nyathi',
+      age: 15,
+      email: 'mtha@gmail.com',
     },
     {
-      id: 2,
+      id: 3,
       name: 'Mtha Nyathi',
+      age: 15,
+      email: 'mtha@gmail.com',
     },
   ];
   
   let incrementor = 2;
-  
+  //query students
   const getStudents = () => {
     return {
       students,
     };
   };
-  
+//add new student & autou iincrement id
   const postStudents = (newStudent) => {
     const student = {
       id: incrementor,
-      name: newStudent.name
+      name: newStudent.name,
+      age: newStudent.age,
+      email: newStudent.email
     };
   
     students.push(student);
@@ -30,6 +37,7 @@ const students = [
     return students; 
   };
 
+  //find id and change name
   const putStudents = (id, newStudent) => {
   const studentIndex = students.findIndex(student => student.id === id);
 
@@ -40,6 +48,7 @@ const students = [
   return students;
 };
 
+//delete student
 const deleteStudents = (id) => {
   const studentIndex = students.findIndex(student => student.id === id);
 
